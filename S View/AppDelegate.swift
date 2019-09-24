3af4f9b0fb67890a308195464f4b15658ba52877
Emir Sarı <emirsari@gmail.com>
2019-09-24 17:13:53 +0300

    initial commit

---
+++ S View/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  S View
+//
+//  Created by Emir SARI on 24.09.2019.
+//  Copyright © 2019 Emir SARI. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ S View/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  S View
+//
+//  Created by Emir SARI on 24.09.2019.
+//  Copyright © 2019 Emir SARI. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
